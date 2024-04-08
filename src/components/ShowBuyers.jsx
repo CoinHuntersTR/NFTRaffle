427dda7221a33c1dcfe12d3d03fc590e31068e24
srknssmn <srknssmn@gmail.com>
2024-04-08 23:33:47 +0300

    update

--- src/components/ShowBuyers.jsx
+++ src/components/ShowBuyers.jsx
@@ -1,10 +1,14 @@
 function ShowBuyers({buyer}) {
 
+    let first = buyer.slice(0, 5)
+    let last = buyer.slice(-5)
+    let playerAddress = first + "..........." + last
+
     return ( 
         <div>
             <div className="flex flex-row space-x-4">
                 <h3>Buyer:</h3>
-                <p>{buyer}</p>
+                <p>{playerAddress}</p>
             </div>
         </div>
      );
